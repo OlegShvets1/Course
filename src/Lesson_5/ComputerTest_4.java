310d113ebb00484dd1cde3cda9677e7859b71b7a
Oleh <olehpost69@gmail.com>
2022-08-21 20:08:56 +0300

    asd

---
+++ src/Lesson_5/ComputerTest_4.java
@@ -0,0 +1,10 @@
+package Lesson_5;
+
+public class ComputerTest_4 {
+    public static void main(String[] args) {
+        Computer_4 computer_4 = new Computer_4();
+        computer_4.system(21);
+        computer_4.system(32, "asdasd", 3479);
+
+    }
+}
